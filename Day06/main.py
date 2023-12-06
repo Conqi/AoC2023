7b400a58a42777a213616057bc6d133ac04c7715
Lars Göring <lars.goering@live.de>
2023-12-06 10:07:31 +0100

    Better solution for day 6

--- Day06/main.py
+++ Day06/main.py
@@ -1,34 +1,26 @@
-times, distances = open("Day06/input").read().split("\n")
+lines = open("Day06/input").read().split("\n")
 
-gigatime = times.split(':')[1].split()
-gigadistance = distances.split(':')[1].split()
-gigatime = "".join(gigatime)
-gigadistance = "".join(gigadistance)
+gigatime = int("".join(lines[0].split()[1:]))
+gigadistance = int("".join(lines[1].split()[1:]))
 
-times = [int(x) for x in times.split(':')[1].split()]
-distances = [int(x) for x in distances.split(':')[1].split()]
-winning_times = []
+times = [int(x) for x in lines[0].split()[1:]]
+distances = [int(x) for x in lines[1].split()[1:]]
 solution = 1
 
-times.append(int(gigatime))
-distances.append(int(gigadistance))
-
-for time in times:
-    number_is_greater_than_threshold = False
+for time, distance in zip(times, distances):
     winning_amount = 0
-    for seconds in range(time):
+    for seconds in range(time+1):
         distance_travelled = (time-seconds)*(seconds)
-        if distance_travelled > distances[times.index(time)]:
-            winning_amount += 1
-            number_is_greater_than_threshold = True
-        elif number_is_greater_than_threshold == True:
+        if distance_travelled > distance:
+            winning_amount = time - (seconds * 2) + 1
             break
-    winning_times.append(winning_amount)
+    solution *= winning_amount
 
-winning_times.pop()
+for seconds in range(gigatime+1):
+        distance_travelled = (gigatime-seconds)*(seconds)
+        if distance_travelled > gigadistance:
+            winning_amount = gigatime - (seconds * 2) + 1
+            break
 
-for entries in winning_times:
-    solution *= entries
-    
 print(solution)
 print(winning_amount)
