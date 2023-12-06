de58b5e0f2b83b137879cbb639a4840429b10a67
Lars Göring <lars.goering@live.de>
2023-12-06 17:04:45 +0100

    Implemented faster solution for day 6

--- Day06/main.py
+++ Day06/main.py
@@ -1,36 +1,30 @@
 lines = open("Day06/input").read().split("\n")
 
-gigatime = int("".join(lines[0].split()[1:]))
-gigadistance = int("".join(lines[1].split()[1:]))
-
 times = [int(x) for x in lines[0].split()[1:]]
 distances = [int(x) for x in lines[1].split()[1:]]
 
-solution = 1
+gigatime = int("".join(lines[0].split()[1:]))
+gigadistance = int("".join(lines[1].split()[1:]))
+
+solution1 = 1
+
+def minimum_finder(time, distance):
+    start_second = int(time/2)
+    lowest_maximum = int(time/2)
+    highest_minimum = 0
+    while True:
+        distance_travelled = (time-start_second)*(start_second)
+        if distance_travelled > distance:
+            lowest_maximum = start_second
+            start_second = int((lowest_maximum+highest_minimum)/2)
+        else:
+            highest_minimum = start_second
+            start_second = int((lowest_maximum+highest_minimum)/2)
+        if start_second == lowest_maximum or start_second == highest_minimum:
+            return lowest_maximum
 
 for time, distance in zip(times, distances):
-    winning_amount = 0
-    for seconds in range(time+1):
-        distance_travelled = (time-seconds)*(seconds)
-        if distance_travelled > distance:
-            winning_amount = time - (seconds * 2) + 1
-            break
-    solution *= winning_amount
+    solution1 *= time-(2*minimum_finder(time, distance)-1)
 
-start_second = int(gigatime/2)
-
-#find start time as tenth of gigatime
-while True:
-    distance_travelled = (gigatime-start_second)*(start_second)
-    if distance_travelled < gigadistance:
-        break
-    start_second = int(start_second-(start_second/100))
-
-for seconds in range(start_second, int(gigatime/2)):
-        distance_travelled = (gigatime-seconds)*(seconds)
-        if distance_travelled > gigadistance:
-            winning_amount = gigatime - (seconds * 2) + 1
-            break
-
-print(solution)
-print(winning_amount)
+print(solution1)
+print(gigatime-(2*minimum_finder(gigatime, gigadistance)-1))
