dcbd9c76267548229e825e3e98db6737d05c76a3
Lars Göring <lars.goering@live.de>
2023-12-09 22:28:52 +0100

    Removed unnecessary bloat from day 9

--- Day09/main.py
+++ Day09/main.py
@@ -1,4 +1,4 @@
-def computeNextSequence(sequence, p2):
+def computeNextSequence(sequence):
     global solution2
     global soy2
     helper2 = 0
@@ -16,7 +16,7 @@ def computeNextSequence(sequence, p2):
     if loop_ended == False:
         steps = [next_number - current_number for current_number, next_number in zip(sequence, sequence[1:])]
         soy2.append(sequence[1] - sequence[0])
-        difference = computeNextSequence(steps, p2)
+        difference = computeNextSequence(steps)
 
     return sequence[-1] + difference
 
@@ -24,9 +24,9 @@ solution1 = 0
 solution2 = 0
 soy2 = []
 
-for input in open("Day09/biginput"):
+for input in open("Day09/input"):
     line = list(map(int, input.split()))
-    solution1 += computeNextSequence(line, False)
+    solution1 += computeNextSequence(line)
 
 print(solution1)
 print(solution2)
