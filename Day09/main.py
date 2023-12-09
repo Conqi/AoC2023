ba4cf948f04040e95eb9904703e12c69506ddc1f
Lars Göring <lars.goering@live.de>
2023-12-09 22:27:16 +0100

    Optimized day 9

--- Day09/main.py
+++ Day09/main.py
@@ -1,40 +1,32 @@
-import time
-start = time.time()
-
-solution2 = 0
-
-def computeNextBiggerSequence(sequence):
+def computeNextSequence(sequence, p2):
+    global solution2
+    global soy2
+    helper2 = 0
+    if not soy2:
+        soy2.append(sequence[0])
     loop_ended = False
     if all(numbers == 0 for numbers in sequence):
         loop_ended = True
         difference = 0
+        for entries in reversed(soy2):
+            helper2 = entries - helper2
+        solution2 += helper2
+        soy2.clear()
 
     if loop_ended == False:
         steps = [next_number - current_number for current_number, next_number in zip(sequence, sequence[1:])]
-        difference = computeNextBiggerSequence(steps)
+        soy2.append(sequence[1] - sequence[0])
+        difference = computeNextSequence(steps, p2)
 
     return sequence[-1] + difference
 
-def computeNextSmallerSequence(sequence):
-    loop_ended = False
-    if all(numbers == 0 for numbers in sequence):
-        loop_ended = True
-        difference = 0
-
-    if loop_ended == False:
-        steps = [next_number - current_number for current_number, next_number in zip(sequence, sequence[1:])]
-        difference = computeNextSmallerSequence(steps)
-
-    return sequence[0] - difference
-
 solution1 = 0
 solution2 = 0
+soy2 = []
 
-for input in open("Day09/input"):
+for input in open("Day09/biginput"):
     line = list(map(int, input.split()))
-    solution1 += computeNextBiggerSequence(line)
-    solution2 += computeNextSmallerSequence(line)
+    solution1 += computeNextSequence(line, False)
 
 print(solution1)
 print(solution2)
-print(time.time() - start)
